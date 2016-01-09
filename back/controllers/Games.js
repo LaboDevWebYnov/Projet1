098d9f5c8229df65aae82d635260bfd62380958d
amaitre <antoine.maitre@ynov.com>
2016-01-09 11:59:45 +0100

    Added routes getGames, addGame to Game API

--- back/controllers/Games.js
+++ back/controllers/Games.js
@@ -6,13 +6,14 @@
 var logger = require('log4js').getLogger('controller.Games'),
     mongoose = require('mongoose'),
     gameDB = require('../models/GameDB'),
+    sanitizer = require('sanitizer'),
     Game = mongoose.model('Game');
 
-module.exports.getGames = function getGames (req, res, next) {
+//Path : /games
+module.exports.getGames = function getGames(req, res, next) {
     logger.info('Getting all games from db...');
-    // Code necessary to consume the Weather API and respond
-    Game.find({
-    }, function(err, result) {
+    // Code necessary to consume the Game API and respond
+    Game.find({}, function (err, result) {
         if (err) {
             return next(err.message);
         }
@@ -22,36 +23,58 @@ module.exports.getGames = function getGames (req, res, next) {
     });
 };
 
-module.exports.getGameById = function getGameById (req, res, next) {
-    logger.info('Getting all games from db...');
-    // Code necessary to consume the Weather API and respond
-    Game.find({
-    }, function(err, result) {
-        if (err) {
-            return next(err.message);
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(result || {}, null, 2));
-    });
-}
-
-module.exports.addGame = function addGame (req, res, next) {
+//Path : /games/addGame
+module.exports.addGame = function addGame(req, res, next) {
     logger.info('Adding new user...');
-    // Code necessary to consume the Weather API and respond
+    // Code necessary to consume the Game API and respond
+    var game = new Game({
+        name: sanitizer.escape(req.body.name),
+        releaseDate: sanitizer.escape(req.body.releaseDate),
+        multiPlayer: sanitizer.escape(req.body.multiPlayer),
+        description: sanitizer.escape(req.body.description),
+        editor: sanitizer.escape(req.body.editor),
+        created_at: sanitizer.escape(req.body.created_at),
+        updated_at: sanitizer.escape(req.body.updated_at)
 
-    res.json({"message":"not implemented yet"});
+    });
 
-    //@TODO implement the function
+    game.save(function (err, game) {
+        if (err)
+            return next(err.message);
+
+        res.json({
+            game: game,
+            success: true,
+            status: 200
+
+        });
+
+    });
 };
 
-module.exports.getGameById = function getGameById (req, res, next) {
-    logger.info('BaseUrl:'+req.originalUrl);
-    logger.info('Path:'+req.path);
-    logger.info('Getting the user with id:'+ req.params[0]);
+// Path : /games/getGameById/{gameId}
+module.exports.getGameById = function getGameById(req, res, next) {
+    logger.info('Getting all games from db...');
+    // Code necessary to consume the Game API and respond
+    Game.find({}, function (err, result) {
+        if (err) {
+            return next(err.message);
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(result || {}, null, 2));
+    });
+};
+
+
+// Path : /games/getGameByName/{gameId}
+module.exports.getGameByName = function getGameByName(req, res, next) {
+    logger.info('BaseUrl:' + req.originalUrl);
+    logger.info('Path:' + req.path);
+    logger.info('Getting the user with id:' + req.params[0]);
     // Code necessary to consume the Weather API and respond
 
-    res.json({"message":"not implemented yet"});
+    res.json({"message": "not implemented yet"});
 
     //@TODO implement the function
 };
--- back/controllers/Users.js
+++ back/controllers/Users.js
@@ -6,7 +6,6 @@
 var logger = require('log4js').getLogger('Users'),
     mongoose = require('mongoose'),
     sanitizer = require('sanitizer'),
-    Url = require('url'),
     Util = require('./util.js'),
     UserDB = require('../models/UserDB'),
     User = mongoose.model('User'),
@@ -14,11 +13,10 @@ var logger = require('log4js').getLogger('Users'),
     Address = mongoose.model('Address');
 
 //Path : /users
-module.exports.getUsers = function getUsers (req, res, next) {
+module.exports.getUsers = function getUsers(req, res, next) {
     logger.info('Getting all users from db...');
-    // Code necessary to consume the Weather API and respond
-    User.find({
-    }, function(err, result) {
+    // Code necessary to consume the User API and respond
+    User.find({}, function (err, result) {
         if (err) {
             return next(err.message);
         }
@@ -29,9 +27,9 @@ module.exports.getUsers = function getUsers (req, res, next) {
 };
 
 //Path : /users/addUser
-module.exports.addUser = function addUser (req, res, next) {
+module.exports.addUser = function addUser(req, res, next) {
     logger.info('Adding new user...');
-    // Code necessary to consume the Weather API and respond
+    // Code necessary to consume the User API and respond
     var user = new User({
         firstname: sanitizer.escape(req.body.username),
         lastname: sanitizer.escape(req.body.lastname),
@@ -45,44 +43,34 @@ module.exports.addUser = function addUser (req, res, next) {
         friends: []
     });
 
-    user.save(function(err, user){
-        if(err){
-            res.json({
-                success: false,
-                status: err.status || 500,
-                err: err
-            });
-        }else {
-            res.json({
-                user: user,
-                success: true,
-                status: 200
+    user.save(function (err, user) {
+        if (err)
+            return next(err.message);
 
-            });
-        }
+        res.json({
+            user: user,
+            success: true,
+            status: 200
+
+        });
+
     });
 };
 
 // Path : /users/getUserById/{userId}
-module.exports.getUserById = function getUserById (req, res, next) {
-    logger.info('BaseUrl:'+req.originalUrl);
-    logger.info('Path:'+req.path);
+module.exports.getUserById = function getUserById(req, res, next) {
+    logger.info('BaseUrl:' + req.originalUrl);
+    logger.info('Path:' + req.path);
 
-    logger.info('Getting the user with id:'+ Util.getPathParams(req)[3]);
-    // Code necessary to consume the Weather API and respond
+    logger.info('Getting the user with id:' + Util.getPathParams(req)[3]);
+    // Code necessary to consume the User API and respond
 
     User.findById(
         Util.getPathParams(req)[3],
         function (err, user) {
-            if(err)
-            {
-                logger.info(err.message);
-                res.json({
-                    succes: true,
-                    status: err.status || 500,
-                    err: err
-                });
-            }
+            if (err)
+                return next(err.message);
+
             logger.info(user);
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(user || {}, null, 2));
@@ -91,32 +79,26 @@ module.exports.getUserById = function getUserById (req, res, next) {
 };
 
 // Path : /users/getUserByUsername/{username}
-module.exports.getUserByUsername = function getUserByUsername (req, res, next) {
-    logger.info('BaseUrl:'+req.originalUrl);
-    logger.info('Path:'+req.path);
+module.exports.getUserByUsername = function getUserByUsername(req, res, next) {
+    logger.info('BaseUrl:' + req.originalUrl);
+    logger.info('Path:' + req.path);
 
-    logger.info('Getting the user with id:'+ Util.getPathParams(req)[3]);
-    // Code necessary to consume the Weather API and respond
+    logger.info('Getting the user with id:' + Util.getPathParams(req)[3]);
+    // Code necessary to consume the User API and respond
 
     User.findOne(
-        { username: Util.getPathParams(req)[3] },
+        {username: Util.getPathParams(req)[3]},
         function (err, user) {
-            if(err)
-            {
-                logger.info(err.message);
-                res.json({
-                    succes: true,
-                    status: err.status || 500,
-                    err: err
-                });
-            }
+            if (err)
+                return next(err.message);
+
             logger.info(user);
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify(user || {}, null, 2));
         }
-    )
+    );
+};
 // Path : /users/{username}
-module.exports.updateUser = function updateUser (req, res, next) {
+module.exports.updateUser = function updateUser(req, res, next) {
 
-    };
 };
