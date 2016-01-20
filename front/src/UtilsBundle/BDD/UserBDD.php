70b2e887768e032ce0bdf915bb90c720853ff61d
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2016-01-20 19:16:21 +0100

    GetUser by Id et name implémenter

--- front/src/UtilsBundle/BDD/UserBDD.php
+++ front/src/UtilsBundle/BDD/UserBDD.php
@@ -6,16 +6,61 @@
  * Time: 22:47
  */
 
-namespace UtilsBundle;
+namespace UtilsBundle\BDD;
 
 use GuzzleHttp;
+use UtilsBundle\Utils\User;
 
 class UserBDD
 {
     public static function getUsers(){
         $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'http://localhost:3000/api/users');
-        $user=json_decode($res->getBody());
-        return $user;
+        $res = $client->request('GET', 'http://localhost:3100/api/users');
+        $users=json_decode($res->getBody(),true);
+        $usersArray=array();
+        foreach($users as $user) {
+            $usersArray[] = self::createUser($user);
+        }
+        return $usersArray;
+
+    }
+
+    public static function getUserByid($id){
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', 'http://localhost:3100/api/users/'.$id.'/getUserById');
+        $user=json_decode($res->getBody(),true);
+        return self::createUser($user);
+    }
+
+    public static function getUserByName($name){
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', 'http://localhost:3100/api/users/'.$name.'/getUserByUsername');
+        $user=json_decode($res->getBody(),true);
+        return self::createUser($user);
+    }
+
+    public static function addUser($user){
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('POST', 'http://localhost:3100/api/users/addUser',array("user"=>json_encode($user)));
+        $user=json_decode($res->getBody(),true);
+        return self::createUser($user);
+    }
+
+    public static function createUser($userMongo){
+        return new User($userMongo["_id"],
+            $userMongo["firstname"],
+            $userMongo["lastname"],
+            $userMongo["username"],
+            $userMongo["birthDate"],
+            $userMongo["email"],
+            $userMongo["password"],
+            $userMongo["address"],
+            $userMongo["phoneNumber"],
+            $userMongo["admin"],
+            $userMongo["friends"],
+            $userMongo["created_at"],
+            $userMongo["updated_at"],
+            $userMongo["interests"],
+            $userMongo["active"]);
     }
 }
