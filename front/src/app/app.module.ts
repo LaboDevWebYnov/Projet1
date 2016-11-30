974b9e93a330a74c39f63a7846ac5e542de88cae
Thomas Doret <thomas.doret33@gmail.com>
2016-11-30 16:42:55 +0100

    [SKELETON] add skeleton site

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -11,6 +11,12 @@ import {TestSecurityServiceComponent} from './component/test_service_component/t
 import {TestPlayerAccountServiceComponent} from './component/test_service_component/test-player-account-service/test-player-account-service.component';
 import { TestGameServiceComponent } from './component/test_service_component/test-game-service/test-game-service.component';
 import { TestAddressServiceComponent } from './component/test_service_component/test-address-service/test-address-service.component';
+import { MenuComponent } from './component/shared/menu/menu.component';
+import { LogoComponent } from './component/shared/logo/logo.component';
+import { NewsComponent } from './component/news/news/news.component';
+import { HomeComponent } from './component/home/home/home.component';
+import { EventsComponent } from './component/events/events/events.component';
+import { StreamingsComponent } from './component/streamings/streamings/streamings.component';
 
 
 @NgModule({
@@ -21,13 +27,42 @@ import { TestAddressServiceComponent } from './component/test_service_component/
     TestGameServiceComponent,
     TestSecurityServiceComponent,
     TestPlayerAccountServiceComponent,
-    TestAddressServiceComponent
+    TestAddressServiceComponent,
+    MenuComponent,
+    LogoComponent,
+    NewsComponent,
+    HomeComponent,
+    EventsComponent,
+    StreamingsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'news',
+        component: NewsComponent
+      },
+      {
+        path: 'streamings',
+        component: StreamingsComponent
+      },
+      {
+        path: 'events',
+        component: EventsComponent
+      },
+      /*ROUTE DE TEST POUR LES SERVICES*/
       {
         path: 'users',
         component: TestUserServiceComponent
